1eade4b329aecde1ea640295906e8e51adf35e5f
rahmadaniii11 <153922124+rahmadaniii11@users.noreply.github.com>
2025-09-17 16:20:09 -0700

    Add files via upload

---
+++ inventaris.sql
@@ -0,0 +1,291 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2025 at 09:40 AM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `inventaris`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `idbarang` int(11) NOT NULL,
+  `merek_id` int(11) NOT NULL,
+  `kategori_id` int(11) NOT NULL,
+  `nama_barang` varchar(128) NOT NULL,
+  `keterangan` varchar(256) NOT NULL,
+  `harga` decimal(20,0) NOT NULL,
+  `kadaluarsa` date NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang`
+--
+
+INSERT INTO `barang` (`idbarang`, `merek_id`, `kategori_id`, `nama_barang`, `keterangan`, `harga`, `kadaluarsa`, `stok`) VALUES
+(1, 0, 0, 'Printer', 'Printer Canon Baru', '1200000', '2025-09-15', 2),
+(3, 0, 0, 'Printer', 'Printer Epson', '0', '0000-00-00', 2),
+(4, 0, 0, 'Spidol', ' ', '0', '0000-00-00', 10),
+(5, 0, 0, 'Printer', 'Printer Epson L3210', '0', '0000-00-00', 0),
+(6, 0, 0, 'babab', 'xscfggxhcd', '0', '0000-00-00', 12),
+(13, 0, 0, 'kusuka', 'makanan ringan', '0', '2025-09-17', 12),
+(20, 13, 5, 'sosis', 'cemilan berbahab daging', '1000', '2027-11-22', 24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_keluar`
+--
+
+CREATE TABLE `barang_keluar` (
+  `idbarang_keluar` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` varchar(256) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang_keluar`
+--
+
+INSERT INTO `barang_keluar` (`idbarang_keluar`, `barang_id`, `jumlah`, `keterangan`, `tanggal`) VALUES
+(1, 1, 1, 'Rusak', '2020-11-26'),
+(2, 5, 2, 'Peminjaman Printer - Budi', '2024-05-25'),
+(3, 7, 2, 'bcdcjdnknv.mlzmxc,xzm', '2025-09-09'),
+(4, 20, 10, 'rusak', '2025-09-14');
+
+--
+-- Triggers `barang_keluar`
+--
+DELIMITER $$
+CREATE TRIGGER `kurang_stok` AFTER INSERT ON `barang_keluar` FOR EACH ROW BEGIN
+	UPDATE barang SET stok = stok - new.jumlah WHERE idbarang = new.barang_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang_masuk`
+--
+
+CREATE TABLE `barang_masuk` (
+  `idbarang_masuk` int(11) NOT NULL,
+  `barang_id` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `keterangan` varchar(256) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `barang_masuk`
+--
+
+INSERT INTO `barang_masuk` (`idbarang_masuk`, `barang_id`, `jumlah`, `keterangan`, `tanggal`) VALUES
+(1, 4, 10, 'Beli Baru', '2020-11-24'),
+(2, 1, 3, 'Beli baru', '2020-11-23'),
+(3, 3, 2, 'Beli baru', '2020-11-24'),
+(4, 5, 2, 'Pengembalian Printer - Budi', '2024-05-25'),
+(5, 6, 12, 'aku\r\n', '2025-09-02'),
+(6, 7, 20, 'baru di tambahkan', '2025-10-01'),
+(7, 7, 12, 'bdhbdnmcbxmc,mxncmdnv,d.,', '2025-09-09'),
+(8, 8, 22, 'kdkdm', '2025-09-09'),
+(9, 13, 12, 'banyak', '2025-09-10'),
+(10, 16, 155, 'bahan', '2025-09-10'),
+(11, 20, 34, 'banyak di sukai', '2025-09-12'),
+(12, 21, 80, 'minuman ', '2025-09-12');
+
+--
+-- Triggers `barang_masuk`
+--
+DELIMITER $$
+CREATE TRIGGER `tambah_stok` AFTER INSERT ON `barang_masuk` FOR EACH ROW BEGIN
+	UPDATE barang SET stok = stok + new.jumlah WHERE idbarang = new.barang_id;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `idkategori` int(11) NOT NULL,
+  `nama_kategori` varchar(128) NOT NULL,
+  `keterangan` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`idkategori`, `nama_kategori`, `keterangan`) VALUES
+(1, 'Elektronik', 'Barang Elektronik'),
+(4, 'Alat Tulis', 'Pensil, Bolpoin, Spidol'),
+(5, 'cemilan', 'makanan ringan'),
+(6, 'bumbu dapur', 'bahan masak'),
+(7, 'minuman', 'air bersoda');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `merek`
+--
+
+CREATE TABLE `merek` (
+  `idmerek` int(11) NOT NULL,
+  `nama_merek` varchar(128) NOT NULL,
+  `keterangan` varchar(256) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `merek`
+--
+
+INSERT INTO `merek` (`idmerek`, `nama_merek`, `keterangan`) VALUES
+(7, 'Epson', 'Printer'),
+(8, 'Canon', 'Printer'),
+(9, 'Nikon', 'Camera'),
+(10, 'Asus', 'PC\r\n'),
+(12, 'Snowman', 'Spidol, Bolpoin'),
+(13, 'indofood', 'snek\r\n'),
+(14, 'nescafe', 'minum');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id_users` int(5) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `no_hp` varchar(15) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `level` enum('admin') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id_users`, `nama`, `no_hp`, `username`, `password`, `level`) VALUES
+(3, 'Administrator', '081000111000', 'admin', '$2y$10$E33mbIeZc665JZiGOIwCMunuLcI.YnlIzMvGoqgPWflEykvFGFTAK', 'admin'),
+(4, 'rahmadani', '082271204647', 'rahma', '$2y$10$eIWBcjLAdZTh.KAZrwHgMO87cvdfCjvWmW2hOgeLuj8.qO9NxmSOK', 'admin');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`idbarang`);
+
+--
+-- Indexes for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD PRIMARY KEY (`idbarang_keluar`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  ADD PRIMARY KEY (`idbarang_masuk`),
+  ADD KEY `barang_id` (`barang_id`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`idkategori`);
+
+--
+-- Indexes for table `merek`
+--
+ALTER TABLE `merek`
+  ADD PRIMARY KEY (`idmerek`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_users`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `idbarang` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- AUTO_INCREMENT for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  MODIFY `idbarang_keluar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `barang_masuk`
+--
+ALTER TABLE `barang_masuk`
+  MODIFY `idbarang_masuk` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `kategori`
+--
+ALTER TABLE `kategori`
+  MODIFY `idkategori` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
+-- AUTO_INCREMENT for table `merek`
+--
+ALTER TABLE `merek`
+  MODIFY `idmerek` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_users` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `barang_keluar`
+--
+ALTER TABLE `barang_keluar`
+  ADD CONSTRAINT `barang_keluar_ibfk_1` FOREIGN KEY (`barang_id`) REFERENCES `barang_masuk` (`barang_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
